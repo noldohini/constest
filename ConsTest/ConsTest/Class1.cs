ab85fe69efdddb71f447e2ebc0dd0e94ffd84dfb
noldohini <noldohini@gmail.com>
2018-04-05 00:43:03 +0300

    Add files via upload

---
+++ ConsTest/ConsTest/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsTest
+{
+    public class Class1
+    {
+        static int GetRandom()
+        {
+            Random rnd = new Random();
+            int value = rnd.Next(1, 10);
+            return value;
+        }
+        public static void Main()
+        {
+            Console.Write("N = ");
+            string nVar = Console.ReadLine();
+            int nVarConverted = Convert.ToInt32(nVar);
+            int iter = 1;
+            while (iter <= nVarConverted)
+            {
+                Console.WriteLine(GenerateString(iter));
+                iter++;
+            }
+        }
+        static string GenerateString(int iter)
+        {
+            var chars = "ABCDEFGHIKJKLMNOPQRSTUVWXYZ";
+            Random rnd = new Random(iter);
+            int i = 0;
+            int charQuan = rnd.Next(1, 10);
+            string myString = "";
+            while (i <= charQuan)
+            {
+                if (i < charQuan)
+                {
+                    myString = myString + chars[rnd.Next(chars.Length)] + "+";
+                }
+                else
+                {
+                    myString = myString + chars[rnd.Next(chars.Length)];
+                }
+                i++;
+            }
+            return myString;
+        }
+    }
+}
